5d6b0010ee646af77fd1ff6640c21677ae8c63c7
Mark Amoah <mcprah@gmail.com>
2023-07-20 02:18:24 +0000

    chore: setup app colors and add image assets

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const light = Color(0xFFFFFFFF);
+  static const primary = Color(0xFF101C26);
+  static const secondary = Color(0xFFB5BDC5);
+  static const secondaryLight = Color(0xFFF7F7F7);
+  static const secondaryLight2 = Color(0xFFF1F1F1);
+  static const accent = Color(0xFFF7E353);
+  static const success = Color(0xFF00C744);
+  static const danger = Color(0xFFEB000E);
+  static const info = Color(0xFF289DFE);
+}
